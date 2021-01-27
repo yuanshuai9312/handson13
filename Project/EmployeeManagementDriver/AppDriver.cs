4d7b021f433d0f715e0f3f76271f22ba3eddf441
yuan.shuai <yuanshuai9312@gmail.com>
2021-01-28 00:02:00 +0900

    first commit

---
+++ Project/EmployeeManagementDriver/AppDriver.cs
@@ -0,0 +1,27 @@
+﻿using Codeer.Friendly;
+using Codeer.Friendly.Dynamic;
+using Codeer.Friendly.Windows;
+using Codeer.Friendly.Windows.Grasp;
+using Ong.Friendly.FormsStandardControls;
+using System.Diagnostics;
+
+namespace EmployeeManagementDriver
+{
+    public class AppDriver
+    {
+        WindowsAppFriend _app;
+        public MainFormDriver MainForm { get; private set; }
+
+        public AppDriver()
+        {
+            var process = Process.Start("EmployeeManagement.exe");
+            _app = new WindowsAppFriend(process);
+            MainForm = new MainFormDriver(new WindowControl(_app, process.MainWindowHandle));
+        }
+
+        public void Release()
+        {
+            Process.GetProcessById(_app.ProcessId).CloseMainWindow();
+        }
+    }
+}
---
+++ Project/TestScenario/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmployeeManagementDriver;
+using Codeer.Friendly;
+using Codeer.Friendly.Dynamic;
+using Codeer.Friendly.Windows.NativeStandardControls;
+using System.Diagnostics;
+using System.Collections.Generic;
+using EmployeeManagement;
+
+namespace TestScenario
+{
+    [TestClass]
+    public class AdjustDriver
+    {
+        AppDriver _app;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _app = new AppDriver();
+
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _app.Release();
+        }
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            var addForm = _app.MainForm.ButtonAdd_EmulateClick();
+            addForm.TextBoxName.EmulateChangeText("ishikawa-tatsuya");
+            addForm.TextBoxAddress.EmulateChangeText("Japan");
+            addForm.RadioButtonMan.EmulateCheck();
+            addForm.ButtonEntry.EmulateClick();
+            Assert.AreEqual("ishikawa-tatsuya(男) Japan", _app.MainForm.ListBoxEmployee_GetItemText(0));
+        }
+
+        [TestMethod]
+        public void TestError()
+        {
+            var addForm = _app.MainForm.ButtonAdd_EmulateClick();
+            addForm.TextBoxName.EmulateChangeText("ishikawa-tatsuya");
+            addForm.TextBoxAddress.EmulateChangeText("Japan");
+            Assert.AreEqual("性別を入力してください。", addForm.ButtonEntry_EmulateClickAndGetMessage());
+            addForm.Close();
+        }
+
+        [TestMethod]
+        public void TestAddShortcut()
+        {
+            List<EmployeeData> data = new List<EmployeeData>();
+            for (int i = 0; i < 10000; i++)
+            {
+                data.Add(new EmployeeData()
+                {
+                    Name = "Name" + i.ToString(),
+                    Address = "Osaka-" + i.ToString(),
+                    IsMan = i % 2 == 0
+                });
+            }
+            _app.MainForm.AddEmployeeData(data.ToArray());
+        }
+    }
+}
